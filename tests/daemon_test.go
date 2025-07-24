7b10c9c5891f78d2d41337bf8ac449d989845d99
Pelayo Maojo <pelayo.mao@gmail.com>
2025-07-24 12:05:49 +0100

    build(testutil): compile mock server once

--- tests/daemon_test.go
+++ tests/daemon_test.go
@@ -18,17 +18,27 @@ import (
 )
 
 var stopServer func()
+var cleanup func()
 
 func TestMain(m *testing.M) {
 	var err error
+	cleanup, err = testutil.BuildMockServer()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 	stopServer, err = testutil.StartMockServer()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		cleanup()
 		os.Exit(1)
 	}
 	code := m.Run()
 	if stopServer != nil {
 		stopServer()
+	}
+	if cleanup != nil {
+		cleanup()
 	}
 	os.Exit(code)
 }
